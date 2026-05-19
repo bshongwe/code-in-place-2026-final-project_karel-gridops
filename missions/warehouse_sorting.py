acb44ffd9cc512dd437da4f86478a94c223f00bb
bshongwe <dru.da.beat.hustle@gmail.com>
2026-05-19 10:17:28 +0200

    add warehouse sorting mission implementation

---
+++ missions/warehouse_sorting.py
@@ -0,0 +1,43 @@
+from karel.stanfordkarel import *
+from engine.navigation import return_home
+from engine.beeper_logic import collect_all_beepers
+
+
+"""
+Warehouse Sorting Mission
+-------------------------
+Karel traverses warehouse aisles,
+collects inventory,
+and returns to the dock.
+"""
+
+
+
+def main():
+    process_warehouse()
+    return_home()
+
+
+
+def process_warehouse():
+    for i in range(4):
+        process_aisle()
+
+        if left_is_clear():
+            move_to_next_aisle()
+
+
+
+def process_aisle():
+    while front_is_clear():
+        collect_all_beepers()
+        move()
+
+    collect_all_beepers()
+
+
+
+def move_to_next_aisle():
+    turn_left()
+    move()
+    turn_left()
