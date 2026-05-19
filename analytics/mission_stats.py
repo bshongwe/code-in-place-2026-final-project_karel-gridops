e299446f48928c2847d808d1ede42c0d6a34592a
bshongwe <dru.da.beat.hustle@gmail.com>
2026-05-19 11:12:20 +0200

    initialize mission statistics tracking with move, beeper collection, and mission completion functions

---
+++ analytics/mission_stats.py
@@ -0,0 +1,26 @@
+mission_stats = {
+    'moves': 0,
+    'beepers_collected': 0,
+    'missions_completed': 0
+}
+
+
+
+def record_move():
+    mission_stats['moves'] += 1
+
+
+
+def record_beeper_collection():
+    mission_stats['beepers_collected'] += 1
+
+
+
+def complete_mission():
+    mission_stats['missions_completed'] += 1
+
+
+
+def print_stats():
+    print('Mission Statistics')
+    print(mission_stats)
