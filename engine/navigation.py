9a81bdb955438779d1dc3543bceb7cb808711615
bshongwe <dru.da.beat.hustle@gmail.com>
2026-05-19 10:21:21 +0200

    implement navigation functions for Karel robot

---
+++ engine/navigation.py
@@ -0,0 +1,54 @@
+from karel.stanfordkarel import *
+
+
+
+def turn_right():
+    for i in range(3):
+        turn_left()
+
+
+
+def turn_around():
+    turn_left()
+    turn_left()
+
+
+
+def move_until_wall():
+    while front_is_clear():
+        move()
+
+
+
+def face_north():
+    while not_facing_north():
+        turn_left()
+
+
+
+def face_south():
+    while not_facing_south():
+        turn_left()
+
+
+
+def face_east():
+    while not_facing_east():
+        turn_left()
+
+
+
+def face_west():
+    while not_facing_west():
+        turn_left()
+
+
+
+def return_home():
+    face_west()
+    move_until_wall()
+
+    face_south()
+    move_until_wall()
+
+    face_east()
